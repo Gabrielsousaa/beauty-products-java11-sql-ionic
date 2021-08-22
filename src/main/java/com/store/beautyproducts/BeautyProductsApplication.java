f68202443fcea800b21c9aa4853880101b360352
Gabriel de Sousa Souto <44745576+Gabrielsousaa@users.noreply.github.com>
2021-08-22 13:55:26 -0300

    projeto teste

---
+++ src/main/java/com/store/beautyproducts/BeautyProductsApplication.java
@@ -0,0 +1,13 @@
+package com.store.beautyproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/beautyproducts/BeautyProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.beautyproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

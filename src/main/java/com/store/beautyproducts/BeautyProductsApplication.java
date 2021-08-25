b26005822b3f96a07dff7f985ce0887d9ce74638
Gabriel de Sousa Souto <44745576+Gabrielsousaa@users.noreply.github.com>
2021-08-24 21:42:46 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/store/beautyproducts/BeautyProductsApplication.java
+++ src/main/java/com/store/beautyproducts/BeautyProductsApplication.java
@@ -1,13 +1,24 @@
 package com.store.beautyproducts;
 
+import com.store.beautyproducts.services.S3Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeautyProductsApplication {
+public class BeautyProductsApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
 	public static void main(String[] args) {
 		SpringApplication.run(BeautyProductsApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\lTHINGl\\Pictures\\transferir.jpg");
+	} 
 
 }
